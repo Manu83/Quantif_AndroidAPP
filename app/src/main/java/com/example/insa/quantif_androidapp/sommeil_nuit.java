43ec79a1b30c2d947469379a6da9398ce38c5b8d
RoyLDD <yohangracia@gmail.com>
2017-11-18 15:18:13 +0100

    [YG] Mini modif

--- app/src/main/java/com/example/insa/quantif_androidapp/sommeil_nuit.java
+++ app/src/main/java/com/example/insa/quantif_androidapp/sommeil_nuit.java
@@ -3,11 +3,11 @@ package com.example.insa.quantif_androidapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class analyse_nuit extends AppCompatActivity {
+public class sommeil_nuit extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_analyse_nuit);
+        setContentView(R.layout.activity_sommeil_nuit);
     }
 }
