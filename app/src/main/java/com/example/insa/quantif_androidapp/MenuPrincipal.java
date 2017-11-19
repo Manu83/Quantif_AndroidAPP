0908d18e60f677318ae0fc1313da2018b953ede0
RoyLDD <yohangracia@gmail.com>
2017-11-19 12:02:35 +0100

    [YG] Menu Principal fini

--- app/src/main/java/com/example/insa/quantif_androidapp/MenuPrincipal.java
+++ app/src/main/java/com/example/insa/quantif_androidapp/MenuPrincipal.java
@@ -13,6 +13,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MenuPrincipal extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -34,6 +36,17 @@ public class MenuPrincipal extends AppCompatActivity
             }
         });
         */
+
+        //Bouton "Démarer une activité"
+        final ImageButton demarerActivite = (ImageButton) findViewById(R.id.imageButton8);
+        demarerActivite.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MenuPrincipal.this, sport_selection.class);
+                startActivity(intent);
+            }
+        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
