ccf2339b7a15d5cd4a39ab9b807e4a9aabc1ae4f
RoyLDD <yohangracia@gmail.com>
2017-11-19 12:48:17 +0100

    [YG] Sport Selec en cours

--- app/src/main/java/com/example/insa/quantif_androidapp/sport_selection.java
+++ app/src/main/java/com/example/insa/quantif_androidapp/sport_selection.java
@@ -1,7 +1,10 @@
 package com.example.insa.quantif_androidapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class sport_selection extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class sport_selection extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sport_selection);
+
+        final ImageButton objectif = (ImageButton) findViewById(R.id.imageButton2);
+        objectif.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(sport_selection.this, sport_objectif.class);
+                startActivity(intent);
+            }
+        });
     }
 }
