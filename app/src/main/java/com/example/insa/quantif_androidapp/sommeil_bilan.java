693d1435d29e1a39b0677a8de79a53180edf10d0
RoyLDD <yohangracia@gmail.com>
2017-11-21 23:14:57 +0100

    [YG] Sport ok et pages sommeil crées

---
+++ app/src/main/java/com/example/insa/quantif_androidapp/sommeil_bilan.java
@@ -0,0 +1,28 @@
+package com.example.insa.quantif_androidapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class sommeil_bilan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setTitle("Bilan de la nuit");
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sommeil_bilan);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        finish();
+        return true;
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return true;
+    }
+}
+
