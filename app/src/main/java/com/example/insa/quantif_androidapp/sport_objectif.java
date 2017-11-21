0a6e6555edd0196cadc5febb739e912934c4c360
Manuel Amouroux <manuel.amouroux@insa-lyon.fr>
2017-11-21 15:41:25 +0100

    bilan semaine (sommeil)

--- app/src/main/java/com/example/insa/quantif_androidapp/sport_objectif.java
+++ app/src/main/java/com/example/insa/quantif_androidapp/sport_objectif.java
@@ -1,11 +1,14 @@
 package com.example.insa.quantif_androidapp;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageButton;
 
 public class sport_objectif extends AppCompatActivity {
@@ -26,6 +29,29 @@ public class sport_objectif extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        final EditText time = findViewById(R.id.editText);
+
+        final TextWatcher mTextEditorWatcher = new TextWatcher() {
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            public void afterTextChanged(Editable s)
+            {
+                if( ( s.length()==2 | s.length()==5 ) && ( s.charAt(s.length() -1 ) | s.charAt(s.length() -1 ) ) != ':')
+                    s.append(':');
+            }
+        };
+
+        time.addTextChangedListener(mTextEditorWatcher);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
